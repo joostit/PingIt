4c9e9f460b70daaadbc2bc9c5470732cc70b50ae
Joost Haverkort <dev@joostit.com>
2023-09-15 14:43:05 +0200

    Added ALL wildcard for crossorigins

--- src/main/java/com/joostit/pingit/PingItApplication.java
+++ src/main/java/com/joostit/pingit/PingItApplication.java
@@ -50,7 +50,7 @@ public class PingItApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200", CorsConfiguration.ALL));
 		corsConfiguration.setAllowedHeaders(List.of("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));

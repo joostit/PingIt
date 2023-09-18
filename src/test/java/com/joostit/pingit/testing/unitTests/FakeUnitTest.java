e15f4c7dabcdd9a7cfaf0e87ee1e04f836d3432c
Joost Haverkort <dev@joostit.com>
2023-09-18 08:16:11 +0200

    Started working on adding tests

---
+++ src/test/java/com/joostit/pingit/testing/unitTests/FakeUnitTest.java
@@ -0,0 +1,19 @@
+package com.joostit.pingit.testing.unitTests;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FakeUnitTest {
+
+    @Test
+    void FakeUnitTestTestA(){
+        assertThat(true);
+    }
+
+    @Test
+    void FakeUnitTestTestB(){
+        assertThat(true);
+    }
+
+
+}
